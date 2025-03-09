dfa4037597581dfef47e0a20fd300a886ae5222c
6E776C7A <patpaj3@gmail.com>
2025-03-09 16:01:19 +0100

    Zadanie 1

--- Lab01/Zad1.py
+++ Lab01/Zad1.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
-import matplotlib
 import numpy as np
 from singen import singen
+from scipy.signal import periodogram
 
 # A
 amplA = 230
@@ -143,23 +143,53 @@ plt.title("Porówanie cosinus 95,105.")
 plt.grid()
 plt.show()
 
-# D
-matplotlib.use('TkAgg')
-fs = pow(10, 3)
+# D 1
+fs1 = pow(10, 3)
 fb = 50
 fm = 1
 df = 5
 b = df/fm
 time = 1
-t_size = int(time*fs)
+t_size = int(time*fs1)
 t = np.linspace(0, time, t_size, endpoint=True)
 sin = b*np.sin(2*np.pi*fm*t)
 SFM = np.cos(2*np.pi*fb*t + b*np.sin(2*np.pi*fm*t))
-sin_dm = np.sin(2*np.pi*fb*t)
+sin_dm = np.cos(2*np.pi*fb*t)
 plt.plot(t, sin, label="Modulujący")
 plt.plot(t, SFM, label="Po modulacji")
 plt.plot(t, sin_dm, label="Przed modulacji")
-plt.plot()
+plt.legend()
+plt.grid()
+plt.show()
+
+# D 2
+
+fs2 = 25
+t_size = int(time*fs2)
+t2 = np.linspace(0, time, t_size, endpoint=True)
+SFM2 = np.cos(2*np.pi*fb*t2 + b*np.sin(2*np.pi*fm*t2))
+error = SFM - np.interp(t, t2, SFM2)
+plt.plot(t2, SFM2, label="Mniejsze próbkowanie 25Hz")
+plt.plot(t, SFM, label="Próbkowanie 10kHz")
+plt.legend()
+plt.grid()
+plt.show()
+plt.plot(t, error, label="Błędy modulacji")
+plt.legend()
+plt.grid()
+plt.show()
+
+# D 3
+
+freq1, power1 = periodogram(SFM, fs1)
+freq2, power2 = periodogram(SFM2, fs2)
+
+plt.plot(freq1, power1, label="Widmowa gęstość przed zmianą próbkowania")
+plt.xlim(0, 100)
+plt.legend()
+plt.grid()
+plt.show()
+plt.plot(freq2, power2, label="Widmowa gęstść mocy po zmianie próbkowania")
 plt.legend()
 plt.grid()
 plt.show()
